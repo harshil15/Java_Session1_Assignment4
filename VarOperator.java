6a3b596f4767246def97a771365e2b500ba93dfb
Harshil <harshil.chhajed12@gmail.com>
2016-04-24 10:50:48 +0530

    Assignment 4
    Session 1 Assignment 4

---
+++ VarOperator.java
@@ -0,0 +1,19 @@
+package acadgild;
+
+public class VarOperator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = 5;
+		int y =10;
+		int a =  (x+y*2);
+		int b = (x-y+2);
+		int c = (x+y)*2;
+		int d = y%x;
+		System.out.println("x+y*2  :: "+ a);
+		System.out.println("x+y*2  :: "+ b );
+		System.out.println("x+y*2  :: "+ c);
+		System.out.println("x+y*2  :: "+ d);
+	}
+
+}
